fb054777ca65ac7438a279df2e5f48ee4c59426d
Samantha  Torres <34137147+SamanthaT27@users.noreply.github.com>
2018-11-11 17:50:19 -0600

    Made changes to login and register buttons
    Login button works to open a new scene but register does not work yet. There still needs to be changes made to registerpage.fxml and reigsterpagecontroller.java and fxmldocumnetcontroller.java

--- AdvisingAssistant2/src/advisingassistant2/FXMLDocumentController.java
+++ AdvisingAssistant2/src/advisingassistant2/FXMLDocumentController.java
@@ -9,27 +9,78 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Sammy
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private Button openLogin;
+      @FXML
+    private TextField UsernameBox;
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private PasswordField PasswordBox;
+
+    @FXML
+    private Text UsernameLabel;
+
+    @FXML
+    private Text PasswordLabel;
+
+    @FXML
+    private Text WelcomeLabel;
+
+    @FXML
+    private Button resgisterButton;
+
+    @FXML
+    void handleButtonAction(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("loginPage.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setOpacity(1);
+            stage.setTitle("Login Page");
+            stage.setScene(new Scene(root1,450,450));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Can't load Login Window");
+        }
+
     }
-    
+     @FXML
+    void openRegistration(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("registerPage.fxml"));
+            Parent root2 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setOpacity(1);
+            stage.setTitle("Registration Page");
+            stage.setScene(new Scene(root2,450,450));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Can't load Register Window");
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
