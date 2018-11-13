45704a37dc622096138e7c11c169ea672f586d2f
Samantha  Torres <34137147+SamanthaT27@users.noreply.github.com>
2018-11-12 18:59:09 -0600

    Working Register Button
    This is the clean working welcome page with login and register buttons fully functioning

--- AdvisingAssistant2/src/Register/RegisterPageController.java
+++ AdvisingAssistant2/src/Register/RegisterPageController.java
@@ -7,9 +7,14 @@ package Register;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -19,11 +24,27 @@ import javafx.scene.control.RadioButton;
 
  
 public class RegisterPageController implements Initializable {
-    
-   @FXML
+
+    @FXML
+    private TextField First_Name_TxtBx;
+    @FXML
+    private TextField Last_name_TxtBx;
+    @FXML
+    private TextField ID_TxtBx;
+    @FXML
     private RadioButton StudentRadioButton;
     @FXML
     private RadioButton StaffRadioButton;
+    @FXML
+    private Button Submit_button;
+    @FXML
+    private Label ID_label;
+    @FXML
+    private Label DM_label;
+    @FXML
+    private Text First_Name_Label;
+    
+   
     
     /**
      * Initializes the controller class.
@@ -32,5 +53,13 @@ public class RegisterPageController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void StudentRadioAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void StaffRadioAction(ActionEvent event) {
+    }
     
 }
