71bffbbfcfdfa3c2e12ec5b87e7fdd4ec9d37e35
Matthew R. Garza <matthewgarza14@yahoo.com>
2018-12-05 13:40:29 -0600

    Recommendation Tab Done
    It pulls the courses recommended to complete the general core and major degree plan.

---
+++ AdvisingAssistant2/src/LoggedIn/Recommended.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LoggedIn;
+
+/**
+ *
+ * @author xINSANITYx
+ */
+public class Recommended {
+    
+    private String subject;
+    private String courseNum;
+    private String credit;
+    private String courseName;
+
+    public Recommended(String subject, String courseNum, String credit, String courseName) {
+        this.subject = subject;
+        this.courseNum = courseNum;
+        this.credit = credit;
+        this.courseName = courseName;
+    }
+
+    /**
+     * @return the subject
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject the subject to set
+     */
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * @return the courseNum
+     */
+    public String getCourseNum() {
+        return courseNum;
+    }
+
+    /**
+     * @param courseNum the courseNum to set
+     */
+    public void setCourseNum(String courseNum) {
+        this.courseNum = courseNum;
+    }
+
+    /**
+     * @return the credit
+     */
+    public String getCredit() {
+        return credit;
+    }
+
+    /**
+     * @param credit the credit to set
+     */
+    public void setCredit(String credit) {
+        this.credit = credit;
+    }
+
+    /**
+     * @return the courseName
+     */
+    public String getCourseName() {
+        return courseName;
+    }
+
+    /**
+     * @param courseName the courseName to set
+     */
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+    
+    
+}
